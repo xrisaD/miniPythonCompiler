2319a22a143b34b88a73ac5d983b9a195421f818
Nick Smyr <smyrnioudis.nikolaos@outlook.com>
2020-12-26 08:19:29 +0200

    final grammar version

--- examples/example4.py
+++ examples/example4.py
@@ -1,2 +1,3 @@
 print 15 + (3 + 2) * 4 + 4 **2 * 4 + (b[5 + 3 ] + hello())
-print true and false and true or true or true and false and 5 > 3 or true or 15 == 2222222222222222222222222222222222222222222222222222222222
+if true and false and true or true or true and false and 5 > 3 or true or 15 == 2222222222222222222222222222222222222222222222222222222222:
+    print yes
